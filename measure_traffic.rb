ff3e74bda6199175c8b0dc8daecf72e057ebc284
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-09-28 19:35:16 +0000

    Create measure_traffic.rb

---
+++ measure_traffic.rb
@@ -0,0 +1,37 @@
+require "Selection"
+
+a = evaluate_malicious_users(0.676, 0.324, 0.16, 275)
+
+get_statistics(:webcomic,  "Uploaded Fairy has #{1 - @malicious_human}, #{@malicious_human} malicious users, and #{@malicious_bots} bot traffic.",
+               :anticheat, "Bequest Antichat has #{1 - @malicious_human}, #{@malicious_human} malicious users, and #{@malicious_bots} bot traffic.",
+               :selection, "Selection has #{1 - @malicious_human}, #{@malicious_human} malicious users, and #{@malicious_bots} bot traffic.")
+
+# Remove one of these to reduce confidence.
+reasses
+reasses
+reasses
+reasses
+
+b = evaluate_malicious_users(0.710526316, 0.289473684, 0.16, 39)
+
+get_statistics(:webcomic,  "Uploaded Fairy has #{1 - @malicious_human}, #{@malicious_human} malicious users, and #{@malicious_bots} bot traffic.",
+               :anticheat, "Bequest Antichat has #{1 - @malicious_human}, #{@malicious_human} malicious users, and #{@malicious_bots} bot traffic.",
+               :selection, "Selection has #{1 - @malicious_human}, #{@malicious_human} malicious users, and #{@malicious_bots} bot traffic.")
+
+# Remove one of these to reduce confidence.
+reasses
+reasses
+reasses
+reasses
+
+c = evaluate_malicious_users(0.56626506, 0.43373494, 0.16, 166)
+
+get_statistics(:webcomic,  "Uploaded Fairy has #{1 - @malicious_human}, #{@malicious_human} malicious users, and #{@malicious_bots} bot traffic.",
+               :anticheat, "Bequest Antichat has #{1 - @malicious_human}, #{@malicious_human} malicious users, and #{@malicious_bots} bot traffic.",
+               :selection, "Selection has #{1 - @malicious_human}, #{@malicious_human} malicious users, and #{@malicious_bots} bot traffic.")
+
+# Remove one of these to reduce confidence.
+reasses
+reasses
+reasses
+reasses
